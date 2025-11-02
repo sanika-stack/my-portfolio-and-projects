fee143957182a71383debb4d2633fe82554fbc25
sanika-stack <sanudalvi575@gmail.com>
2025-11-03 01:21:24 +0530

    employee/
    Added Employee management project files.

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected Successfully!"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the EMS Backend!");
+});
+
+app.listen(PORT, () => console.log('🚀 Server running on port ${PORT}'));
